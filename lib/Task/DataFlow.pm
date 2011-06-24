9a703e51b49ec76da160f0edbd258f612301303f
Alexei Znamensky <russoz@cpan.org>
2011-06-24 13:02:51 -0300

    Initial commit
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

---
+++ lib/Task/DataFlow.pm
@@ -0,0 +1,32 @@
+package Task::DataFlow;
+
+use strict;
+use warnings;
+
+# ABSTRACT: The DataFlow Module Collection (new flavor)
+
+# VERSION
+
+
+1;
+
+=pod
+
+=pkgroup DataFlow Core
+
+=pkg DataFlow 1.111750 First version with split modules
+
+=pkgroup DataFlow Converters
+
+=pkg DataFlow::Proc::MessagePack 1.111750
+
+=pkg DataFlow::Proc::JSON 1.111750
+
+=pkg DataFlow::Proc::YAML 1.111750
+
+=pkgroup DataFlow for Web
+
+=pkg DataFlow::Proc::URLRetriever 1.111750
+
+=cut
+
